6dcf41ccc280107cccabb788efe49e9203d57a34
Deepak Goswami <125822925+luckygoswami@users.noreply.github.com>
2025-02-25 17:24:03 +0530

    Chapter 6 completed

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -30,7 +30,7 @@ async function seedUsers() {
 }
 
 async function seedInvoices() {
-  await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+  // await sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
 
   await sql`
     CREATE TABLE IF NOT EXISTS invoices (
